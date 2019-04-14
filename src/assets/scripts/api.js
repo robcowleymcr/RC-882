fdc34396490644eecfbd8d0abfc59377dc0a17ea
Rob Cowley <rob.cowley@magairports.com>
2019-04-14 09:08:55 +0100

    Applied async/await to loadSound func

--- src/assets/scripts/api.js
+++ src/assets/scripts/api.js
@@ -15,21 +15,23 @@ export default {
       src: 'wav/hat.wav'
     }
   ],
-  loadSound (url, index) {
-    axios.get(url, {
-      responseType: 'arraybuffer',
-      headers: {
-        'Accept': 'audio/wav'
-      }
-    }).then((response) => {
-      console.log(this.array[index])
-      this.array[index].buffer = response.data
-    })
+  async loadSound (url, index) {
+    try {
+      const arrayBuffer = await axios.get(url, {
+        responseType: 'arraybuffer',
+        headers: {
+          'Accept': 'audio/wav'
+        }
+      })
+      this.array[index].buffer = arrayBuffer.data
+    } catch (error) {
+      console.log(error)
+    }
   },
   loadSounds () {
     this.array.forEach((obj, index) => {
       this.loadSound(obj.src, index)
     })
-    console.log('finished loop')
+    console.log(this.array)
   }
 }
